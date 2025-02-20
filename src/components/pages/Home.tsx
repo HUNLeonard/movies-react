5171d3ce1b596f978b7302f1e0af853d40962ada
Attila <acsata18@gmail.com>
2025-02-20 12:41:00 +0100

    Fix overflow Hero

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <main className={cn("pb-16 min-h-screen *:mx-auto *:max-w-[90rem]",
       "space-y-8 bg-[url('../yellow-200-bg.svg')] bg-repeat-y bg-cover")}>
-      <section className={cn("min-w-screen *:mx-auto *:max-w-[90rem]",
+      <section className={cn("min-w-full *:mx-auto *:max-w-[90rem]",
         "pt-16 sm:pb-2 md:pt-16 lg:pb-12 lg:pt-24",
         "bg-[url('../moviebg1.webp')] bg-no-repeat bg-cover",
         "[box-shadow:0_0_20px_0_rgba(0,0,0,1)] bg-[#d28c69]"
